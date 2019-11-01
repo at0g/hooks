1607129df17d9a7f8250d13db60013323f6c2764
Ben Daley <benjamindaley@gmail.com>
2019-11-02 04:26:40 +1100

    refactor useOffline

--- src/useOffline.js
+++ src/useOffline.js
@@ -1,16 +1,10 @@
 import { useLayoutEffect, useMemo, useState } from 'react'
 
-export default function useOffline(fallback = false) {
-    const [offline, setOffline] = useState(fallback)
-
-    if (typeof window === 'undefined') {
-        return offline
-    }
-
+function useEvents(setState) {
     const eventListeners = useMemo(() => {
         const handles = {
-            offline: () => setOffline(true),
-            online: () => setOffline(false),
+            offline: () => setState(true),
+            online: () => setState(false),
         }
         return {
             listen: () => {
@@ -24,15 +18,26 @@ export default function useOffline(fallback = false) {
                 }
             },
         }
-    }, [setOffline])
+    }, [setState])
+
+    return eventListeners
+}
+
+export default function useOffline(fallback = false) {
+    const [offline, setOffline] = useState(fallback)
+
+    if (typeof window === 'undefined') {
+        return offline
+    }
+
+    const events = useEvents(setOffline)
 
     useLayoutEffect(() => {
         if (typeof navigator !== 'undefined' && navigator.onLine === false) {
             setOffline(true)
         }
-        const removeListeners = eventListeners.listen()
-        return removeListeners
-    }, [eventListeners])
+        return events.listen()
+    }, [events])
 
     return offline
 }
